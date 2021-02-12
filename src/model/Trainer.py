61d0689ee8adfeb72433a2dbb2ce9cba90df12d0
Osman Demirel <osmandemirel1905@gmail.com>
2021-02-12 23:33:23 +0300

    Update Trainer.py

--- src/model/Trainer.py
+++ src/model/Trainer.py
@@ -1,33 +1,26 @@
 import os
 import torch
+from torch import nn
 from torch import optim
 from pathlib import Path
 from src.data.dataset import ECGDataset
 
 
 class Trainer:
-    def __init__(self, model, criteria, train_loader, val_loader, config):
+    def __init__(self, model, criteria, train_loader, val_loader, config, treshold=0.3):
         """
         :param model: A deep learning model extends to nn.Module
         :param criteria: A loss function
         :param train_loader: Data loader for training data set
         :param val_loader: Data loader for validation data set
         :param config: A dict data that includes configurations. You can refer to below:
-            conf = {
-                "number_of_epochs": 50,
-                "val_every": 5,
-                "optimizer": "SGD",
-                "lr": 0.004,
-                "device": "gpu",
-                "result_path":"path_string",
-                "batch_size":16
-            }
         """
         self.model = model
         self.train_loader = train_loader
         self.val_loader = val_loader
         self.criteria = criteria
         self.config = config
+        self.treshold = treshold
         self.metrics = {
             "train_loss": [],
             "train_acc": [],
@@ -39,6 +32,7 @@ class Trainer:
             self.model.to(torch.device('cuda:0'))
         self.optimizer = self.__set_optimizer()
         self.RESULT_SAVE_PATH = self.config["result_path"]
+        self.sf = nn.Softmax()
 
     def train_supervised(self,n_epochs=None):
         if n_epochs is None:
@@ -47,8 +41,8 @@ class Trainer:
 
         for epoch in range(n_epochs):
             self.train_epoch()
-            print("Epoch:{} Loss:{} Accuracy:{}".format(epoch, self.metrics["train_loss"][epoch],
-                                                        self.metrics["train_acc"][epoch]))
+            #print("Epoch:{} Loss:{} Accuracy:{}".format(epoch, self.metrics["train_loss"][epoch],
+            #                                            self.metrics["train_acc"][epoch]))
             if (epoch+1) % val_every == 0:
                 self.optimizer.zero_grad(set_to_none=True)
                 self.evaluate()
@@ -58,7 +52,7 @@ class Trainer:
         iter_loss = float(0.0)
         iter_correct_prediction = int(0)
         self.model.train()
-        
+
         for data, label in self.train_loader:
             
             self.optimizer.zero_grad()
@@ -68,18 +62,16 @@ class Trainer:
                 label = label.to(torch.device('cuda:0'))
 
             out = self.model(data)
-
-            if str(self.criteria)=="CrossEntropyLoss()":
-                loss = self.criteria(out, label)
-            else:
-                loss = self.criteria(out.float(), label.float().view(-1, 1))
+            loss = self.criteria(out, label)
 
             loss.backward()
             self.optimizer.step()
 
             iter_loss += float(loss.item())
-            iter_correct_prediction += (torch.max(out.detach(), 1).indices==label).int().sum().item()
-        
+            iter_correct_prediction += (torch.where(out>0.05, 1, 0) == label).sum()
+        print(out)
+        print(iter_loss / len(self.train_loader))
+        print(iter_correct_prediction / (len(self.train_loader) * self.config["batch_size"]))
         self.metrics["train_loss"].append(iter_loss / len(self.train_loader))    
         self.metrics["train_acc"].append(iter_correct_prediction / (len(self.train_loader) * self.config["batch_size"]))
         torch.cuda.empty_cache()
@@ -95,19 +87,13 @@ class Trainer:
                     label = label.to(torch.device('cuda:0'))
 
                 out = self.model(data)
-
-                if str(self.criteria) == "CrossEntropyLoss()":
-                    loss = self.criteria(out, label)
-                else:
-                    loss = self.criteria(out.float(), label.float().view(-1, 1))
+                loss = self.criteria(out, label)
 
                 val_loss += float(loss.item())
-                val_correct_prediction += (torch.max(out.detach(), 1).indices==label).int().sum().item()
-            print("Validation: Loss:{} Accuracy:{}".format(val_loss / len(self.val_loader),
-                                                        val_correct_prediction / len(self.val_loader) / self.config["batch_size"]))
+            # print("Validation: Loss:{}, val_correct_prediction / len(self.val_loader) / self.config["batch_size"]))
 
             self.metrics["val_loss"].append(val_loss / len(self.val_loader))
-            self.metrics["val_acc"].append(val_correct_prediction / (len(self.val_loader) * self.config["batch_size"]))
+            #self.metrics["val_acc"].append(val_correct_prediction / (len(self.val_loader) * self.config["batch_size"]))
 
     def __set_optimizer(self):
         weight_decay = self.config["weight_decay"] if "weight_decay" in self.config else 0
