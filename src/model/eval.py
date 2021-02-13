6c957dfb7cf53495e628052950cab3c1f1119d17
Osman Demirel <osmandemirel1905@gmail.com>
2021-02-13 14:28:35 +0300

    Update eval.py

--- src/model/eval.py
+++ src/model/eval.py
@@ -1,10 +1,9 @@
 import torch
-from src.model.utils import *
 import os
 from pathlib import Path
 
 
-def eval(model,dataloader, criteria, batch_size,gpu_f=True):
+def eval(model,dataloader, criteria, batch_size, gpu_f=True):
     """
     :param model: model to be tested
     :param dataloader: dataloader for testing set
@@ -39,44 +38,14 @@ def eval(model,dataloader, criteria, batch_size,gpu_f=True):
                 label = label.to(torch.device('cuda:0'))
 
             out = model(data)
+            loss = criteria(out, label)
 
-            if str(criteria) == "CrossEntropyLoss()":
-                loss = criteria(out, label)
-            else:
-                loss = criteria(out.float(), label.float().view(-1, 1))
-            # save metrics
-            iter_preds = torch.max(out.detach(), 1).indices
+            iter_preds = torch.where(out>0.05, 1, 0)
 
             preds = torch.cat((preds,iter_preds),dim=0)
             target = torch.cat((target,label),dim=0)
             eval_loss += float(loss.item())
-            eval_correct_prediction += (iter_preds == label).int().sum().item()
+            eval_correct_prediction += (iter_preds == label).sum().item()
         print("Validation: Loss:{} Accuracy:{}".format(eval_loss / len(dataloader)/batch_size,
                                                        eval_correct_prediction / len(dataloader)/batch_size))
     return (preds.cpu().numpy(),target.cpu().numpy())
-
-def eval_scores(preds,targets,path=None,file_name=None):
-    """
-    :param preds: predictions, should be a numpy array
-    :param targets: targets, should be a numpy array
-    :param path: file path to save metrics
-    :param file_name: file name to save metrics
-    :return: return scores as tuple
-        (accuracy,precision,recall,confusion matrix)
-    """
-    assert preds.shape==targets.shape
-    precision = calculate_precision(preds,targets)
-    recall = calculate_recall(preds,targets)
-    accuracy = calculate_accuracy(preds,targets)
-    conf_mat = calculate_confusion_matrix(preds,targets)
-
-    if isinstance(path,str) and isinstance(file_name,str): # save if path and filename are provided
-        with open(os.path.join(path, file_name)+".txt", "w") as file:
-            file.write(str(accuracy) + "\n")
-            file.write(str(precision) + "\n")
-            file.write(str(recall) + "\n")
-            file.write(str(conf_mat) + "\n")
-            file.close()
-            print("Results are written to {}.txt".format(file_name))
-        save_confusion_matrix(conf_mat,"Confusion Matrix",os.path.join(path, file_name))
-    return (accuracy,precision,recall,conf_mat)
